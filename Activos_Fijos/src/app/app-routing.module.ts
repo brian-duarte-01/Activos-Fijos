4bf3fdaae967d385803d9bc80af2996e82971a09
brian-duarte-01 <brianduartemuralles@gmail.com>
2022-10-18 17:32:34 -0600

    v2.0
    primer vista realizada

--- Activos_Fijos/src/app/app-routing.module.ts
+++ Activos_Fijos/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnunciosComponent } from './anuncios/anuncios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dashboard', component: AnunciosComponent},
+  {path:'', redirectTo:'dashboard',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
