2980188e3e3be6df85e535b5cf58b3fd863eb7cf
brian-duarte-01 <81836728+brian-duarte-01@users.noreply.github.com>
2022-10-26 19:06:18 -0600

    v11.4
    Creacion de metodo post en Departamento

--- Activos_Fijos/src/app/api-departamento.service.ts
+++ Activos_Fijos/src/app/api-departamento.service.ts
@@ -1,16 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Departamento } from './Models/DepartamentoModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiDepartamentoService {
 
+  _url = 'https://localhost:44340/api/ACT_DEPARTAMENTO';
+
+
   constructor(
     private http: HttpClient
   ) { }
 
-  get(url:string){
+  get(url: string) {
     return this.http.get(url); //GET  https://localhost:44340/api/ACT_DEPARTAMENTO
   }
+
+  post(url: string, body: { ID_DEPARTAMENTO: string; DEP_NOMBRE: string; }) {
+    return this.http.post(url, body);
+  }
+
+  
+
+
+
+
 }
--- Activos_Fijos/src/app/rdepartamento/rdepartamento.component.ts
+++ Activos_Fijos/src/app/rdepartamento/rdepartamento.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApiDepartamentoService } from '../api-departamento.service';
+import { Departamento } from '../Models/DepartamentoModel';
+
 
 @Component({
   selector: 'app-rdepartamento',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rdepartamento.component.css']
 })
 export class RdepartamentoComponent implements OnInit {
+  listar: any = [];
+  respuesta: any = [];
 
-  constructor() { }
+  form!: FormGroup;
+  constructor(
+    private apiRest: ApiDepartamentoService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      ID_DEPARTAMENTO: ['0'],
+      DEP_NOMBRE: new FormControl('',[Validators.required])
+    });
+
   }
+
+
+  post() {
+    this.apiRest.post('https://localhost:44340/api/ACT_DEPARTAMENTO',
+      {
+        ID_DEPARTAMENTO: '0',
+        DEP_NOMBRE: this.form.value.DEP_NOMBRE
+      }
+    )
+      .subscribe(respuesta => {
+       
+        confirm('se envio correctamente');
+        location.href='departamento';
+        
+      })
+  }
+
+
+
+
+
 
 }
