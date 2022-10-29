1433e3882994f0162d3097ee6feee7c41d23bc95
hramirezs <114131884+hramirezs@users.noreply.github.com>
2022-10-28 21:51:24 -0600

    V13 PDF implementacion

--- Activos_Fijos/src/app/depres/depres.component.ts
+++ Activos_Fijos/src/app/depres/depres.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { DepresService } from '../services/depres.service';
+import jspdf, { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-depres',
@@ -14,10 +16,12 @@ export class DepresComponent implements OnInit {
     private router:Router
   ) {
     console.log("Servicio Depresiaciones")
+    this.downloadPDF();
    }
 
   ngOnInit(): void {
     this.getcategoria();
+    
   }
 
   getcategoria(){
@@ -27,4 +31,30 @@ export class DepresComponent implements OnInit {
 
     })
   }
+
+  downloadPDF() {
+    // Extraemos el
+    const DATA: any = document.getElementById('htmlData');
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    html2canvas(DATA, options).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+
+      // Add image Canvas to PDF
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}_activos_fijos.pdf`);
+    });
+  }
+
 }
